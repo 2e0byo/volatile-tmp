11e3275ca34d6d275dc561f8c3cdbead20587d33
John Maximilian <2e0byo@gmail.com>
2020-10-02 12:37:29 +0100

    some mild cleanup

--- volatile-tmp.py
+++ volatile-tmp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 from shutil import rmtree
 
 tmpdir = os.path.expanduser("~/volatile-tmp")
@@ -10,7 +10,7 @@ protected_files = ["readme.org", ".volatile"]
 
 
 def remove(p):
-    """Remove file/dir by path"""
+    """Remove file/dir by path."""
     print("removing", p)
     try:
         os.remove(p)
@@ -19,7 +19,7 @@ def remove(p):
 
 
 def get_expiry(dir):
-    "read .volatile in dir into timedelta"
+    """read .volatile in dir into timedelta."""
     with open(dir + "/.volatile", "r") as f:
         line = ""
         while line == "":
@@ -27,18 +27,19 @@ def get_expiry(dir):
 
     d = {"weeks": 0, "days": 0, "hours": 0, "minutes": 0, "seconds": 0}
 
-    for i in line.split(','):
+    for i in line.split(","):
         components = i.split("=")
         try:
             d[components[0]] = float(components[1])
         except:
             pass
-    return (now - timedelta(
-        weeks=d['weeks'],
-        days=d['days'],
-        hours=d['hours'],
-        minutes=d['minutes'],
-        seconds=d['seconds']))
+    return now - timedelta(
+        weeks=d["weeks"],
+        days=d["days"],
+        hours=d["hours"],
+        minutes=d["minutes"],
+        seconds=d["seconds"],
+    )
 
 
 expiry = get_expiry(tmpdir)
@@ -46,7 +47,6 @@ expiry = get_expiry(tmpdir)
 # scan
 things = os.scandir(tmpdir)
 for entry in things:
-
     if not entry.is_dir():  # files first
         if entry.name in protected_files:
             continue
@@ -67,7 +67,10 @@ for entry in things:
                 all_files.append(root + "/" + f)
         if len(all_files) == 0:  # cleanup empty dirs
             remove(entry.path)
-        elif datetime.utcfromtimestamp(
-                os.path.getmtime(max(all_files,
-                                     key=os.path.getmtime))) < dir_expiry:
+        elif (
+            datetime.utcfromtimestamp(
+                os.path.getmtime(max(all_files, key=os.path.getmtime))
+            )
+            < dir_expiry
+        ):
             remove(entry.path)
