7be44042696ee38c89eaba8728eb06050ea0f335
John Maximilian <2e0byo@gmail.com>
2022-05-11 16:24:14 +0100

    chore: isort.

--- volatile-tmp.py
+++ volatile-tmp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import logging
 import os
 from datetime import datetime, timedelta
 from shutil import rmtree
-import logging
 from time import perf_counter
 
 logger = logging.getLogger(__name__)
